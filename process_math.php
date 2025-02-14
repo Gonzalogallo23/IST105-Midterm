4a14e74bba8a6bc3b7b4f2a04b6638862095f0aa
Gonzalo Gallo <gonzalogallot23@gmail.com>
2025-02-13 18:21:24 -0800

    changes

--- process_math.php
+++ process_math.php
@@ -4,8 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input2 = escapeshellarg($_POST["input2"]);
     $operation = escapeshellarg($_POST["operation"]);
 
-    // Call Python script with command-line arguments
-    $command = "python3 math_operations.py $input1 $input2 $operation";
+    $command = "python3 math_operation.py $input1 $input2 $operation";
     $output = shell_exec($command);
 
     // Display the output
