166534bd9ae6f71c5a58f2fc068f38bdb55ecd43
Gonzalo Gallo <gonzalogallot23@gmail.com>
2025-02-13 19:31:30 -0800

    last change

--- process_math.php
+++ process_math.php
@@ -4,11 +4,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input2 = escapeshellarg($_POST["input2"]);
     $operation = escapeshellarg($_POST["operation"]);
 
-    $command = "python3 math_operations.py $input1 $input2 $operation";
+    $command = "python3 math_operation.py $input1 $input2 $operation";
     $output = shell_exec($command);
 
-    echo "<pre>$output</pre>"; // Ensure proper formatting
-    echo "<p>This result was processed on my EC2 instance with Public IP: &lt;EC2-Public-IP&gt;</p>";
-    echo "<p>Access the application via Load Balancer URL: <a href='http://&lt;load-balancer-dns&gt;/math_form.php'>http://&lt;load-balancer-dns&gt;/math_form.php</a></p>";
+    echo "<pre>$output</pre>";
 }
 ?>
